a26df27d11ce514a05267860077821fe40ce78bf
smakivic <stefan.makivic@cosylab.com>
2024-12-04 18:51:01 +0100

    added sign out option

--- app/src/main/java/com/example/myapplication/activities/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/activities/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.myapplication.activities
 
+import android.content.Intent
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -7,10 +9,25 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.myapplication.R
+import com.example.myapplication.databinding.ActivityMainBinding
+import com.example.myapplication.utilities.Constants
+import com.example.myapplication.utilities.PreferenceManager
+import android.util.Base64
+import com.google.firebase.firestore.FieldValue
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.messaging.FirebaseMessaging
+import kotlin.io.encoding.ExperimentalEncodingApi
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var preferenceManager: PreferenceManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
+
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -18,7 +35,64 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        preferenceManager = PreferenceManager(applicationContext)
+        loadUserDetails()
+        getToken()
+        setListeners()
+    }
+    private fun setListeners() {
+        binding.imageSignOut.setOnClickListener{ v-> signOut()}
     }
 
+    @OptIn(ExperimentalEncodingApi::class)
+    private fun loadUserDetails(){
+        binding.textName.text = preferenceManager.getString(Constants.KEY_NAME)
+        val bytes = Base64.decode(preferenceManager.getString(Constants.KEY_IMAGE),
+            Base64.DEFAULT)
+        val bitmap = BitmapFactory.decodeByteArray(bytes,0,bytes.size)
+        binding.imageProfile.setImageBitmap(bitmap)
+
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun getToken(){
+        FirebaseMessaging.getInstance().token.addOnSuccessListener { token ->
+            updateToken(token)
+        }
+    }
+
+    private fun updateToken(token: String){
+        val database = FirebaseFirestore.getInstance()
+        val documentReference = database.collection(Constants.KEY_COLLECTION_USERS).document(
+            preferenceManager.getString(Constants.KEY_USER_ID)
+        )
+        documentReference.update(Constants.KEY_FCM_TOKEN, token)
+            .addOnSuccessListener { unused -> showToast("Token updated successfully") }
+            .addOnFailureListener{ e -> showToast("Unable to update token")}
+
+    }
+
+    private fun signOut(){
+        showToast("Signing out...")
+        val database = FirebaseFirestore.getInstance()
+        val documentReference = database.collection(Constants.KEY_COLLECTION_USERS).document(
+            preferenceManager.getString(Constants.KEY_USER_ID)
+        )
+        val updates : HashMap<String, Any> = HashMap()
+        updates.put(Constants.KEY_FCM_TOKEN, FieldValue.delete())
+        documentReference.update(updates)
+            .addOnSuccessListener { unused ->
+                preferenceManager.clear()
+                startActivity(Intent(applicationContext, SignInActivity::class.java))
+                finish()
+            }
+            .addOnFailureListener{ e-> showToast("Unable to sing out")}
+    }
 
 }
